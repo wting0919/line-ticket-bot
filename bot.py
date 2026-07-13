6fdf779c992f29aa2cb917c5097ba677d1f36416
wting0919 <keylovebling@gmail.com>
2026-07-14 00:31:20 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -146,5 +146,10 @@ def handle_message(event):
     )
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
